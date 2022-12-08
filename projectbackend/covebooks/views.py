c69f37f7ee4d94da181b81cb42502301d6ffe634
Ivan <ivanwpang@gmail.com>
2022-12-08 15:43:15 -0800

    added shipping

--- projectbackend/covebooks/views.py
+++ projectbackend/covebooks/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 from django.template import loader
-
-
-
+from django.shortcuts import render
+from .forms import ShippingForm
+from django.http import HttpResponseRedirect
 
 
 def home(request):
@@ -18,7 +18,17 @@ def checkout(request):
   return render(request, 'checkout.html')
 
 def shipping(request):
-  return render(request, 'Shipping.html')
+  submitted = False
+  if request.method == "POST":
+    form = ShippingForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return HttpResponseRedirect('/shipping?submitted = True')
+  else:
+    form = ShippingForm
+    if 'submitted' in request.GET:
+      submitted = True
+  return render(request, 'Shipping.html', {'form':form, 'submitted':submitted})
 
 def harrypotter(request):
   return render(request, 'HarryPotter.html')
