b749895326aff9ca5e772159da96fbc8adfae805
Jakob Tiger <jtigger7010@gmail.com>
2022-12-08 15:40:15 -0800

    login,logout,myprofile,changepassword

--- projectbackend/members/urls.py
+++ projectbackend/members/urls.py
@@ -1,11 +1,16 @@
 
 from django.urls import path
 from . import views
+from .views import UserEditView, PasswordsChangeView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     
     path('login', views.login_user, name='login'),
     path('register', views.register, name='register'),
     path('logout', views.logout_user, name='logout'),
-
+    path('editprofile', views.UserEditView.as_view(), name='editprofile'),
+    path('myprofile', views.myprofile, name='myprofile'),
+    path('password', PasswordsChangeView.as_view(template_name='changepass.html'), name='password'),
+    path('pass_success', views.pass_success, name='passsuccess'),
 ]
